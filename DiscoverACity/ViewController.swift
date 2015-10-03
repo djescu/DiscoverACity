146488210cb915eb3ca544165bf23311b7ddf83e
David Escudero Justel <escu@macbook-air-de-david.local>
2015-10-03 09:21:36 +0200

    Initial Commit

---
+++ DiscoverACity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiscoverACity
+//
+//  Created by David Escudero Justel on 3/10/15.
+//  Copyright © 2015 djescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
